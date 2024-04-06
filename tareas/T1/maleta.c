1cee39a7a987c0fd8506c3e9dd27d46125fd4640
Seba <sebastianvalenzuela@ug.uchile.cl>
2024-04-05 21:54:24 -0300

    Tarea 1

---
+++ tareas/T1/maleta.c
@@ -0,0 +1,69 @@
+#define _XOPEN_SOURCE 500
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+#include "maleta.h"
+
+typedef struct{
+    double *w;
+    double *v;
+    int *z;
+    int n;
+    double maxW;
+    int k;
+    double res;
+} Args;
+
+void *thread(void *p){
+    Args *args = (Args *)p;
+    double *w = args->w;
+    double *v = args->v;
+    int *z = args->z;
+    int n = args->n;
+    double maxW = args->maxW;
+    int k = args->k;
+
+    args->res = llenarMaletaSec(w, v, z, n, maxW, k);
+    return NULL;
+}
+
+double llenarMaletaPar(double w[], double v[], int z[], int n, double maxW, int k){
+    int P = 8;
+    pthread_t pid[P];
+    Args args[P];
+    int resto = k%P;
+
+    for (int i = 0; i < P; i++){
+        args[i].w = w;
+        args[i].v = v;
+        args[i].z = (int *)malloc(n*sizeof(int));
+        args[i].n = n;
+        args[i].maxW = maxW;    
+        args[i].k = k/P;
+        if (resto > 0) {
+            args[i].k += 1;
+            resto--;
+        }
+
+        pthread_create(&pid[i], NULL, thread, &args[i]);
+    }
+
+    double best = -1;
+    for (int i = 0; i < P; i++){
+        pthread_join(pid[i], NULL);
+        if (args[i].res > best){
+            best = args[i].res;
+            for (int j = 0; j < n; j++) {
+                z[j] = args[i].z[j];
+            }
+        }
+        free(args[i].z);
+    }
+    return best;
+}
